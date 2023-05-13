5edc6838943ceebe0013dd40398e6d4e91b4cd6d
sunuazizrahayu <sunuazizrahayu@gmail.com>
2023-05-13 14:26:14 +0700

    update setup nginx

--- debian.sh
+++ debian.sh
@@ -99,23 +99,24 @@ cecho -c 'yellowb' "Install NGINX web server..."
 cecho -c 'green' "Install nginx"
 apt install nginx -y
 
+cecho -c 'green' "update public html"
+mkdir -p /var/www/html/
+rm /var/www/html/*
+wget https://raw.githubusercontent.com/sunuazizrahayu/linux-server/main/nginx/www/index.html -O /var/www/html/index.html
+
+cecho -c 'green' "create new config dir"
+mkdir -p /etc/nginx/conf.d/
+cecho -c 'green' "set default server config on new dir"
+wget https://raw.githubusercontent.com/sunuazizrahayu/linux-server/main/nginx/conf.d/default.conf -O /etc/nginx/conf.d/default.conf
+cecho -c 'green' "set nginx config with new server config dir"
+wget https://raw.githubusercontent.com/sunuazizrahayu/linux-server/main/nginx/nginx.conf -O /etc/nginx/nginx.conf
+
 cecho -c 'green' "remove default config dir"
 rm -rf /etc/nginx/sites-available/
 rm -rf /etc/nginx/sites-enabled/
 
-cecho -c 'green' "create config dir"
-mkdir -p /etc/nginx/conf.d/
-cecho -c 'green' "get default nginx config"
-wget https://raw.githubusercontent.com/sunuazizrahayu/linux-server/main/nginx/nginx.conf -O /etc/nginx/nginx.conf
-cecho -c 'green' "get default server config"
-wget https://raw.githubusercontent.com/sunuazizrahayu/linux-server/main/nginx/conf.d/default.conf -O /etc/nginx/conf.d/default.conf
-
-cecho -c 'green' "prepare public html"
-rm -rf /var/www/html/
-mkdir -p /var/www/html/
-wget https://raw.githubusercontent.com/sunuazizrahayu/linux-server/main/nginx/www/index.html -O /var/www/html/index.html
-
 cecho -c 'green' "reload config nginx"
+nginx -t
 nginx -s reload
 printf "\n"
 
