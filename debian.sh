beb19ef65ce7976722944af3bd04ac49fdf5d9f4
sunuazizrahayu <sunuazizrahayu@gmail.com>
2023-05-13 07:22:50 +0700

    Create debian.sh

---
+++ debian.sh
@@ -0,0 +1,139 @@
+#!/bin/bash
+
+# The following function prints a text using custom color
+# -c or --color define the color for the print. See the array colors for the available options.
+# -n or --noline directs the system not to print a new line after the content.
+# Last argument is the message to be printed.
+cecho () {
+    declare -A colors;
+    colors=(\
+        ['label']='\E[1;33m'\
+        ['black']='\E[0;47m'\
+        ['red']='\E[0;31m'\
+        ['redb']='\E[1;31m'\
+        ['green']='\E[0;32m'\
+        ['yellow']='\E[0;33m'\
+        ['yellowb']='\E[1;33m'\
+        ['blue']='\E[0;34m'\
+        ['magenta']='\E[0;35m'\
+        ['cyan']='\E[0;36m'\
+        ['white']='\E[0;37m'\
+    );
+
+    local defaultMSG="No message passed.";
+    local defaultColor="black";
+    local defaultNewLine=true;
+
+    while [[ $# -gt 1 ]];
+    do
+    key="$1";
+
+    case $key in
+        -c|--color)
+            color="$2";
+            shift;
+        ;;
+        -n|--noline)
+            newLine=false;
+        ;;
+        *)
+            # unknown option
+        ;;
+    esac
+    shift;
+    done
+
+    message=${1:-$defaultMSG};   # Defaults to default message.
+    color=${color:-$defaultColor};   # Defaults to default color, if not specified.
+    newLine=${newLine:-$defaultNewLine};
+
+    echo -en "${colors[$color]}";
+    echo -en "$message";
+    if [ "$newLine" = true ] ; then
+        echo;
+    fi
+    tput sgr0; #  Reset text attributes to normal without clearing screen.
+
+    return;
+}
+
+
+## check for sudo/root
+cecho -c 'label' "Check root access..."
+if ! [ $(id -u) = 0 ]; then
+  echo "This script must run with sudo, try again..."
+  exit 1
+fi
+printf "\n"
+
+
+# update package list
+cecho -c 'label' "Updating package index..."
+apt update
+printf "\n"
+
+# upgrade package
+cecho -c 'label' "Try to Upgrade..."
+apt upgrade
+printf "\n"
+
+# remove apache
+cecho -c 'redb' "Remove default apache..."
+apt purge --auto-remove apache* -y
+printf "\n"
+
+
+# PREPARATION & REQUIREMENTS
+##################################################
+cecho -c 'yellowb' "Install any required packages..."
+apt install \
+  nano curl wget bash-completion lsb-release \
+  ca-certificates gnupg -y
+printf "\n"
+
+
+# INSTALL NGINX
+##################################################
+cecho -c 'yellowb' "Install NGINX web server..."
+# Install
+apt install nginx -y
+printf "\n"
+
+
+cecho -c 'yellowb' "Install Docker..."
+# INSTALL DOCKER: https://docs.docker.com/engine/install/debian/
+##################################################
+# remove old version
+cecho -c 'green' "Remove old Docker..."
+apt remove docker docker-engine docker.io containerd runc -y
+
+# Add Docker’s official GPG key:
+cecho -c 'green' "Add Docker GPG key..."
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+chmod a+r /etc/apt/keyrings/docker.gpg
+
+# Setup Docker Repo
+cecho -c 'green' "Setup Docker Repository..."
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+# Install
+cecho -c 'green' "Update docker package..."
+apt update
+cecho -c 'green' "Installing Docker..."
+apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose docker-compose-plugin -y
+
+# Autorun docker
+cecho -c 'green' "Set autorun Docker..."
+systemctl start docker
+systemctl enable docker
+cecho -c 'yellowb' "Install Docker finish..."
+printf "\n"
+
+# Install Speedtest
+cecho -c 'yellowb' "Install Speedtest..."
+curl -s https://packagecloud.io/install/repositories/ookla/speedtest-cli/script.deb.sh | bash
+apt install speedtest -y
