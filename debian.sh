bfabbed2a2125d023f363c1860d1b71c2c9ef7b6
Sunu Aziz Rahayu <me@kangsunu.com>
2023-05-20 12:36:42 +0700

    update nginx

--- debian.sh
+++ debian.sh
@@ -106,9 +106,9 @@ rm /var/www/html/*
 wget https://raw.githubusercontent.com/sunuazizrahayu/linux-server/main/nginx/www/index.html -O /var/www/html/index.html
 
 cecho -c 'green' "create new config dir"
-mkdir -p /etc/nginx/conf.d/
+mkdir -p /etc/nginx/sites.conf.d/
 cecho -c 'green' "set default server config on new dir"
-wget https://raw.githubusercontent.com/sunuazizrahayu/linux-server/main/nginx/conf.d/default.conf -O /etc/nginx/conf.d/default.conf
+wget https://raw.githubusercontent.com/sunuazizrahayu/linux-server/main/nginx/sites.conf.d/default.conf -O /etc/nginx/sites.conf.d/default.conf
 cecho -c 'green' "set nginx config with new server config dir"
 wget https://raw.githubusercontent.com/sunuazizrahayu/linux-server/main/nginx/nginx.conf -O /etc/nginx/nginx.conf
 
